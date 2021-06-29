ef422b511e9d18436e4b45a749488c5b7ff268f3
Victor Lopes <82065212+VictorLopes1010@users.noreply.github.com>
2021-06-28 23:46:44 -0300

    Projeto!
    Foi feita uma analise e conseguinte um desenvolvimento do projeto de um jogo conhecido, o famoso "Campo Minado"!

---
+++ ivoryit-testeestagio-campo-minado-master/Ivory.TesteEstagio.CampoMinado/ChamaTabuleiro.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ivory.TesteEstagio.CampoMinado
+{
+    class chamatabuleiro : CampoMinado
+    {
+
+        public void Chama()
+        {
+            var campoMinado = new CampoMinado();
+            Console.WriteLine("Início do jogo\n=========");
+            //Declarei as variaveis
+            int escolhaLinha = 0;
+            int escolhaColuna = 0;
+            //Aqui peguei o status, e enquanto a o resultado for diferente de 2 ou 1
+            while (campoMinado.JogoStatus != 2 && campoMinado.JogoStatus != 1)
+            {
+                Console.WriteLine(campoMinado.Tabuleiro);
+                Console.WriteLine("Digite o valor da Linha");
+                escolhaLinha = int.Parse(Console.ReadLine());
+                Console.WriteLine("Digite o valor da Coluna");
+                escolhaColuna = int.Parse(Console.ReadLine());
+                campoMinado.Abrir(escolhaLinha, escolhaColuna);
+                Console.Clear();
+            }
+            //Aqui tratei vitoria/derrota
+            if (campoMinado.JogoStatus == 2)
+            {
+                //Derrota
+                Console.WriteLine(campoMinado.Tabuleiro);
+                Console.WriteLine("Você perdeu");
+
+            }
+            else
+            {
+                //Vitoria
+                Console.WriteLine("Ganhou patrão");
+            }
+
+        }
+    }
+}
---
+++ ivoryit-testeestagio-campo-minado-master/Ivory.TesteEstagio.CampoMinado/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Ivory.TesteEstagio.CampoMinado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Chamei uma classe e instanciei o objeto
+            chamatabuleiro objeto = new chamatabuleiro();
+            objeto.Chama();
+            
+            //Aqui abro o jogo novamente
+            char opcao;
+            Console.WriteLine("1 - para jogar novamente");
+            Console.WriteLine("0 - para fechar");
+            opcao = char.Parse(Console.ReadLine());
+
+            //Função para escolha de fechar ou jogar novamente 
+            switch (opcao)
+            {
+                case '1':
+                    objeto.Chama();
+                    break;
+
+                case '2':
+                    Environment.Exit(0);
+
+                    break;
+
+
+            }
+
+        }
+    }
+}
+
+
+
+
+
+
+
